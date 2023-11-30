c1eacccedbbd7ea0d85a5080d5bb543c31d3d752
Jonathan Jalbert <39426670+jojal5@users.noreply.github.com>
2023-11-30 18:42:05 -0500

    remove test script of gitignore

---
+++ test/script.jl
@@ -0,0 +1,53 @@
+using IDFCurves, Test
+
+using CSV, DataFrames, Distributions, Extremes, ForwardDiff, Gadfly,  LogExpFunctions, Optim
+
+import IDFCurves.IDFdata
+import Distributions: fit_mle, params, quantile
+import Extremes: fit_mle, qqplot
+
+
+
+df = CSV.read(joinpath("data","IDF_702S006.csv"), DataFrame)
+    
+tags = names(df)[2:10]
+durations = [1/12, 1/6, 1/4, 1/2, 1, 2, 6, 12, 24]
+duration_dict = Dict(zip(tags, durations))
+    
+data = IDFdata(df, "Year", duration_dict)
+
+fd = IDFCurves.fit_mle(dGEV, data, 1, [1, 1, 0, .9, 1])
+# fd = IDFCurves.fit_mle_gradient_free(dGEV, data, 1, [1, 1, 0, .9, 1])
+
+m = getdistribution(fd, 24)
+y = getdata(data, "24h")
+
+IDFCurves.qqplot(m, y)
+
+
+
+function hessian(pd::dGEV, data::IDFdata)
+
+    d₀ = duration(pd)
+    θ̂ = [params(fd)...]
+
+    fobj(θ) = -loglikelihood(dGEV(d₀, θ...), data)
+
+    H = ForwardDiff.hessian(fobj, θ̂)
+
+end
+
+H = hessian(fd, data)
+
+
+function quantile(pd::dGEV, d::Real, p::Real)
+    @assert 0<p<1 "The quantile level p must be in (0,1)."
+    @assert d>0 "The duration must be positive."
+
+    marginal = IDFCurves.getdistribution(pd, d)
+
+    return quantile(marginal, p)
+
+end
+
+quantile(fd, 1, .9)
