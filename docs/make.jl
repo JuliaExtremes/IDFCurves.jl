5253bc07e8da0f7445f703ff7f448b3d3228e241
Jonathan Jalbert <39426670+jojal5@users.noreply.github.com>
2024-05-29 08:22:21 -0400

    remove the warning for figure sizes fo Documenter

--- docs/make.jl
+++ docs/make.jl
@@ -4,8 +4,7 @@ CI = get(ENV, "CI", nothing) == "true"
 
 makedocs(sitename = "IDFCurves.jl",
     format = Documenter.HTML(
-    prettyurls = CI,
-    ),
+        prettyurls = CI, size_threshold_warn=10^8 ,size_threshold=10^9, example_size_threshold=10^9),
     pages = [
         "Tutorial" =>["IDF estimation" => "tutorial/idf_estimation.md"],
         "index.md",
