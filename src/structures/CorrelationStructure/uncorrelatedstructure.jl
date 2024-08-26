aca9006a7cf65feffdc6cc25e941df7b74e24f5f
AugustePaoli99 <119446504+AugustePaoli99@users.noreply.github.com>
2024-08-26 12:37:11 +0200

    Revert "Docs/notebook article"

--- src/structures/CorrelationStructure/uncorrelatedstructure.jl
+++ src/structures/CorrelationStructure/uncorrelatedstructure.jl
@@ -19,8 +19,7 @@ end
 
 Construct an UncorrelatedStructure from a set of transformed parameters θ in the real space.
 """
-function construct_model(::Type{<:UncorrelatedStructure}, θ::AbstractVector{<:Any};
-                            final_model::Bool = false)
+function construct_model(::Type{<:UncorrelatedStructure}, θ::AbstractVector{<:Any})
     @assert length(θ) == 0 "The parameter vector length must be 0 for an uncorrelated structure."
 
     return UncorrelatedStructure()
