c099147c87aae90bde7abca440dfba695d0bca76
Jonathan Jalbert <39426670+jojal5@users.noreply.github.com>
2026-04-07 13:51:02 -0400

    reafctor temp.jl

--- test/temp.jl
+++ test/temp.jl
@@ -6,42 +6,8 @@ using Distributions, Test
 using IDFCurves
 
 
-    @testset "rand(::UniversalScaling)" begin
-        
-        pd = UniversalScaling(60, 100, 1, .1, .8, 5, 10)
 
-        n = 3
-        d = [.5, 1, 24]
-        tag = ["10", "11", "12"]
-        x = [10, 11, 12]
-        data = rand(pd, d, n, tags = tag, x = [10, 11, 12])
-
-        @test issetequal(gettag(data), tag)
-        for i in eachindex(tag)
-            @test getduration(data, tag[i]) ≈ d[i]
-            @test getyear(data, tag[i]) == x
-            @test length(getdata(data, tag[i])) == n
-        end
-
-    end
-
-
-pd = UniversalScaling(60, 100, 1, .1, .8, 5, 10)
-
-        n = 3
-        d = [.5, 1, 24]
-        tag = ["10", "11", "12"]
-        x = [10, 11, 12]
-        data = rand(pd, d, n, tags = tag, x = [10, 11, 12])
-
-        @test issetequal(gettag(data), tag)
-        for i in eachindex(tag)
-            @test getduration(data, tag[i]) ≈ d[i]
-            @test getyear(data, tag[i]) == x
-            @test length(getdata(data, tag[i])) == n
-        end
-
-
+# TODO: test fit functions
 
 # TODO Change parameter name
 # \alpha :exponent
