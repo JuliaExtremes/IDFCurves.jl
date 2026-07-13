0b37df2806ace9362830b29ed75d44f33211ff9d
Jonathan Jalbert <39426670+jojal5@users.noreply.github.com>
2026-07-13 16:20:22 -0400

    add more stations in script.jl

--- script.jl
+++ script.jl
@@ -4,7 +4,8 @@
 using Pkg
 pkg"activate ."
 
-using DataFrames, Distributions, IDFCurves, LinearAlgebra, Test
+using DataFrames, Distributions, Extremes, IDFCurves, LinearAlgebra
+using Cairo, Gadfly, Fontconfig
 
 
 # data at Mtl Trudeau
@@ -24,7 +25,7 @@ pvalue = IDFCurves.pvalue(T)
 reject = IDFCurves.decision(T)
 
 # Adjust the p-value for the interduration dependence
-B = 100 # Number of bootstrap samples
+B = 999 # Number of bootstrap samples
 Tstar = IDFCurves.scalingtest_bootstrap(fd, data, B=B)
 Sstar = [Tstar[b].test_statistic for b in eachindex(Tstar)]
 adjusted_pvalue = (1 + count(s -> s >= S, Sstar)) / (B + 1)
@@ -40,87 +41,291 @@ pvalue = IDFCurves.pvalue(T)
 reject = IDFCurves.decision(T)
 
 # Adjust the p-value for the interduration dependence
-B = 100 # Number of bootstrap samples
+B = 999 # Number of bootstrap samples
 Tstar = IDFCurves.scalingtest_bootstrap(fd, data, B=B)
 Sstar = [Tstar[b].test_statistic for b in eachindex(Tstar)]
 adjusted_pvalue = (1 + count(s -> s >= S, Sstar)) / (B + 1)
 
 
 
+train_data = IDFCurves.excludeduration(data, "5min")
+q, _ = Extremes.ecdf(getdata(data, "5min"))
 
+function F(q::AbstractVector{<:Real}, x::Real)
 
-
-
-
-
-
-train_data = excludeduration(data, "5min")
-
-fm = fit_mle(SimpleScaling, train_data, 1)
-
-pd = getdistribution(fm, 5/60)
-
-
-
-
-
-
-
-
-
-
-"""
-    excludeduration(data::IDFdata, tag_out::String)
-
-Remove data of `data` corresponding to the duration identified by `tag_out`.
-"""
-function excludeduration(data::IDFdata, tag_out::String)
-
-    tags = gettag(data)
-
-    if tag_out ∉ tags
-        throw(ArgumentError("tag_out not in the tags, got '$tag_out'."))
-    end
-
-    new_year = Dict{String, Vector{Int64}}()
-    new_data = Dict{String, Vector{Float64}}()
-    new_duration = Dict{String, Float64}()
-
-    new_tag = setdiff(tags, [tag_out])
-
-    for key in new_tag
-        new_year[key] = getyear(data, key)
-        new_data[key] = getdata(data, key)
-        new_duration[key] = getduration(data, key)
-    end
-
-    return IDFdata(new_tag, new_duration, new_year, new_data)
+    # issorted(q) || throw(ArgumentError("quantiles must be sorted"))
+    return count(q .≤ x) / (length(q) + 1. )
 
 end
 
-nd = excludeduration(data, "5min")
+
+ss = IDFCurves.fit_mle(SimpleScaling, train_data, 1)
+
+pd = getdistribution(ss, 5/60)
+
+fig = plot([y->cdf(pd, y), y->F(q, y)], 0, 250,
+    Guide.xlabel("5-min precipitation intensity (mm/h)"),
+    Guide.ylabel("probability"),
+    Theme(key_position=:none)
+    )
+
+draw(PDF("Mtl_cdf_simplescaling.pdf"), fig)
+
+
+gs = IDFCurves.fit_mle(GeneralScaling, train_data, 1)
+
+pd = getdistribution(gs, 5/60)
+
+fig = plot([y->cdf(pd, y), y->F(q, y)], 0, 250,
+    Guide.xlabel("5-min precipitation intensity (mm/h)"),
+    Guide.ylabel("probability"),
+    Theme(key_position=:none)
+    )
+
+draw(PDF("Mtl_cdf_generalscaling.pdf"), fig)
 
 
 
-    tags = ["30min", "1h", "24h"]
-    durations = [0.5, 1, 24]
-    years = [2020, 2021]
-    y = hcat(1:2, 3:4, 5:6)
 
-    d1 = Dict("30min" => 0.5, "1h" => 1., "24h" => 24.)
-    d2 = Dict("30min" => years, "1h" => years, "24h" => years)
-    d3 = Dict("30min" => y[:, 1], "1h" => y[:, 2], "24h" => y[:, 3])
 
-    s = IDFdata(tags, d1, d2, d3)
 
-    @testset "excludeduration(::IDFdata, d)" begin
+# data at Vancouver
+df = IDFCurves.dataset("1108446")
+tags = names(df)[2:10]
+durations = [1/12, 1/6, 1/4, 1/2, 1, 2, 6, 12, 24]
+duration_dict = Dict(zip(tags, durations))
+data = IDFdata(df, "Year", duration_dict)
 
-        @test_throws ArgumentError excludeduration(s, "nonextistant_tag")
+# Fit the Simple Scaling model 
+fd = IDFCurves.fit_mle(SimpleScaling, data, 1.)
 
-        s2 = excludeduration(s, "1h")
-        @test getdata(s2) == Dict("30min" => y[:, 1], "24h" => y[:, 3])
-        @test getduration(s2) == Dict("30min" => 0.5, "24h" => 24.)
-        @test getyear(s2) == Dict("30min" => years, "24h" => years)
-        @test gettag(s2) == ["30min", "24h"]
+# Goodness-of-fit test using the 5min duration as validation
+T = scalingtest(SimpleScaling, data, tag_out = "5min")
+S = T.test_statistic
+pvalue = IDFCurves.pvalue(T)
+reject = IDFCurves.decision(T)
 
-    end
+# Adjust the p-value for the interduration dependence
+B = 999 # Number of bootstrap samples
+Tstar = IDFCurves.scalingtest_bootstrap(fd, data, B=B)
+Sstar = [Tstar[b].test_statistic for b in eachindex(Tstar)]
+adjusted_pvalue = (1 + count(s -> s >= S, Sstar)) / (B + 1)
+
+
+# Fit the General Scaling model
+fd = IDFCurves.fit_mle(GeneralScaling, data, 1.)
+
+# Goodness-of-fit test using the 5min duration as validation
+T = scalingtest(GeneralScaling, data, tag_out = "5min")
+S = T.test_statistic
+pvalue = IDFCurves.pvalue(T)
+reject = IDFCurves.decision(T)
+
+# Adjust the p-value for the interduration dependence
+B = 999 # Number of bootstrap samples
+Tstar = IDFCurves.scalingtest_bootstrap(fd, data, B=B)
+Sstar = [Tstar[b].test_statistic for b in eachindex(Tstar)]
+adjusted_pvalue = (1 + count(s -> s >= S, Sstar)) / (B + 1)
+
+
+
+train_data = IDFCurves.excludeduration(data, "5min")
+q, _ = Extremes.ecdf(getdata(data, "5min"))
+
+function F(q::AbstractVector{<:Real}, x::Real)
+
+    # issorted(q) || throw(ArgumentError("quantiles must be sorted"))
+    return count(q .≤ x) / (length(q) + 1. )
+
+end
+
+
+ss = IDFCurves.fit_mle(SimpleScaling, train_data, 1)
+
+pd = getdistribution(ss, 5/60)
+
+fig = plot([y->cdf(pd, y), y->F(q, y)], 0, 150,
+    Guide.xlabel("5-min precipitation intensity (mm/h)"),
+    Guide.ylabel("probability"),
+    Theme(key_position=:none)
+    )
+
+draw(PDF("Van_cdf_simplescaling.pdf"), fig)
+
+
+gs = IDFCurves.fit_mle(GeneralScaling, train_data, 1)
+
+pd = getdistribution(gs, 5/60)
+
+fig = plot([y->cdf(pd, y), y->F(q, y)], 0, 150,
+    Guide.xlabel("5-min precipitation intensity (mm/h)"),
+    Guide.ylabel("probability"),
+    Theme(key_position=:none)
+    )
+
+draw(PDF("Mtl_cdf_generalscaling.pdf"), fig)
+
+
+
+
+
+
+
+# data at Toronto
+df = IDFCurves.dataset("6158731")
+tags = names(df)[2:10]
+durations = [1/12, 1/6, 1/4, 1/2, 1, 2, 6, 12, 24]
+duration_dict = Dict(zip(tags, durations))
+data = IDFdata(df, "Year", duration_dict)
+
+# Fit the Simple Scaling model 
+fd = IDFCurves.fit_mle(SimpleScaling, data, 1.)
+
+# Goodness-of-fit test using the 5min duration as validation
+T = scalingtest(SimpleScaling, data, tag_out = "5min")
+S = T.test_statistic
+pvalue = IDFCurves.pvalue(T)
+reject = IDFCurves.decision(T)
+
+# Adjust the p-value for the interduration dependence
+B = 999 # Number of bootstrap samples
+Tstar = IDFCurves.scalingtest_bootstrap(fd, data, B=B)
+Sstar = [Tstar[b].test_statistic for b in eachindex(Tstar)]
+adjusted_pvalue = (1 + count(s -> s >= S, Sstar)) / (B + 1)
+
+
+# Fit the General Scaling model
+fd = IDFCurves.fit_mle(GeneralScaling, data, 1.)
+
+# Goodness-of-fit test using the 5min duration as validation
+T = scalingtest(GeneralScaling, data, tag_out = "5min")
+S = T.test_statistic
+pvalue = IDFCurves.pvalue(T)
+reject = IDFCurves.decision(T)
+
+# Adjust the p-value for the interduration dependence
+B = 999 # Number of bootstrap samples
+Tstar = IDFCurves.scalingtest_bootstrap(fd, data, B=B)
+Sstar = [Tstar[b].test_statistic for b in eachindex(Tstar)]
+adjusted_pvalue = (1 + count(s -> s >= S, Sstar)) / (B + 1)
+
+
+
+train_data = IDFCurves.excludeduration(data, "5min")
+q, _ = Extremes.ecdf(getdata(data, "5min"))
+
+function F(q::AbstractVector{<:Real}, x::Real)
+
+    # issorted(q) || throw(ArgumentError("quantiles must be sorted"))
+    return count(q .≤ x) / (length(q) + 1. )
+
+end
+
+
+ss = IDFCurves.fit_mle(SimpleScaling, train_data, 1)
+
+pd = getdistribution(ss, 5/60)
+
+fig = plot([y->cdf(pd, y), y->F(q, y)], 0, 150,
+    Guide.xlabel("5-min precipitation intensity (mm/h)"),
+    Guide.ylabel("probability"),
+    Theme(key_position=:none)
+    )
+
+draw(PDF("Tor_cdf_simplescaling.pdf"), fig)
+
+
+gs = IDFCurves.fit_mle(GeneralScaling, train_data, 1)
+
+pd = getdistribution(gs, 5/60)
+
+fig = plot([y->cdf(pd, y), y->F(q, y)], 0, 150,
+    Guide.xlabel("5-min precipitation intensity (mm/h)"),
+    Guide.ylabel("probability"),
+    Theme(key_position=:none)
+    )
+
+draw(PDF("Tor_cdf_generalscaling.pdf"), fig)
+
+
+
+
+
+using CanadianClimateData
+
+# data at Vancouver
+df = CanadianClimateData.parse_idf_table("//Users/jalbert/Library/CloudStorage/Dropbox/Files/Data/ECCC/ECCC-idf_v3-30_2022_10_31/BC/idf_v3-30_2022_10_31_101_BC_1018621_VICTORIA_INTL_A.txt")
+
+tags = names(df)[2:10]
+durations = [1/12, 1/6, 1/4, 1/2, 1, 2, 6, 12, 24]
+duration_dict = Dict(zip(tags, durations))
+data = IDFdata(df, "Year", duration_dict)
+
+# Fit the Simple Scaling model 
+fd = IDFCurves.fit_mle(SimpleScaling, data, 1.)
+
+# Goodness-of-fit test using the 5min duration as validation
+T = scalingtest(SimpleScaling, data, tag_out = "5min")
+S = T.test_statistic
+pvalue = IDFCurves.pvalue(T)
+reject = IDFCurves.decision(T)
+
+# Adjust the p-value for the interduration dependence
+B = 999 # Number of bootstrap samples
+Tstar = IDFCurves.scalingtest_bootstrap(fd, data, B=B)
+Sstar = [Tstar[b].test_statistic for b in eachindex(Tstar)]
+adjusted_pvalue = (1 + count(s -> s >= S, Sstar)) / (B + 1)
+
+
+# Fit the General Scaling model
+fd = IDFCurves.fit_mle(GeneralScaling, data, 1.)
+
+# Goodness-of-fit test using the 5min duration as validation
+T = scalingtest(GeneralScaling, data, tag_out = "5min")
+S = T.test_statistic
+pvalue = IDFCurves.pvalue(T)
+reject = IDFCurves.decision(T)
+
+# Adjust the p-value for the interduration dependence
+B = 999 # Number of bootstrap samples
+Tstar = IDFCurves.scalingtest_bootstrap(fd, data, B=B)
+Sstar = [Tstar[b].test_statistic for b in eachindex(Tstar)]
+adjusted_pvalue = (1 + count(s -> s >= S, Sstar)) / (B + 1)
+
+
+
+train_data = IDFCurves.excludeduration(data, "5min")
+q, _ = Extremes.ecdf(getdata(data, "5min"))
+
+function F(q::AbstractVector{<:Real}, x::Real)
+
+    # issorted(q) || throw(ArgumentError("quantiles must be sorted"))
+    return count(q .≤ x) / (length(q) + 1. )
+
+end
+
+
+ss = IDFCurves.fit_mle(SimpleScaling, train_data, 1)
+
+pd = getdistribution(ss, 5/60)
+
+fig = plot([y->cdf(pd, y), y->F(q, y)], 0, 150,
+    Guide.xlabel("5-min precipitation intensity (mm/h)"),
+    Guide.ylabel("probability"),
+    Theme(key_position=:none)
+    )
+
+draw(PDF("Tor_cdf_simplescaling.pdf"), fig)
+
+
+gs = IDFCurves.fit_mle(GeneralScaling, train_data, 1)
+
+pd = getdistribution(gs, 5/60)
+
+fig = plot([y->cdf(pd, y), y->F(q, y)], 0, 150,
+    Guide.xlabel("5-min precipitation intensity (mm/h)"),
+    Guide.ylabel("probability"),
+    Theme(key_position=:none)
+    )
+
+draw(PDF("Tor_cdf_generalscaling.pdf"), fig)
