2e04dbf615fb30f37a8a5cb4f4c0fed810dacd4b
Jonathan Jalbert <39426670+jojal5@users.noreply.github.com>
2026-07-20 16:23:01 -0400

    propose refactorization for application.jl

--- application.jl
+++ application.jl
@@ -74,3 +74,105 @@ df_results.valid = valid_computation
 
 CSV.write(joinpath(@__FILE__, filepath, "scalingtest_canadian_stations.csv"), df_results)
 
+
+
+
+
+## Refactored application.jl
+
+
+using LinearAlgebra
+using Random
+
+BLAS.set_num_threads(1)
+
+simple_ok = falses(nstation)
+general_ok = falses(nstation)
+simple_pvalue = fill(NaN, nstation)
+general_pvalue = fill(NaN, nstation)
+valid = trues(nstation)
+
+seeds = rand(Xoshiro(2026), UInt64, nstation)
+
+function assess_station(
+    data::IDFdata;
+    B::Integer=999,
+    level::Real=0.05,
+    rng=Random.default_rng(),
+)
+
+    # Simple Scaling
+    ss_full = fit_mle(SimpleScaling, data, 1.0)
+
+    ss_result = scalingtest_bootstrap_pvalue(
+        ss_full,
+        data;
+        tag_out="5min",
+        B=B,
+        rng=rng,
+    )
+
+    if ss_result.pvalue >= level
+        return (
+            SimpleScaling=true,
+            GeneralScaling=true,
+            SimpleScalingPValue=ss_result.pvalue,
+            GeneralScalingPValue=NaN,
+        )
+    end
+
+    # General Scaling is evaluated only if Simple Scaling is rejected.
+    gs_full = fit_mle(GeneralScaling, data, 1.0)
+
+    gs_result = scalingtest_bootstrap_pvalue(
+        gs_full,
+        data;
+        tag_out="24h",
+        B=B,
+        rng=rng,
+    )
+
+    return (
+        SimpleScaling=false,
+        GeneralScaling=gs_result.pvalue >= level,
+        SimpleScalingPValue=ss_result.pvalue,
+        GeneralScalingPValue=gs_result.pvalue,
+    )
+end
+
+Threads.@threads :dynamic for i in eachindex(filenames)
+
+    try
+        file = joinpath(
+            filepath,
+            "canadian_stations_data",
+            filenames[i],
+        )
+
+        station_df = CSV.read(file, DataFrame)
+        data = IDFdata(station_df, "Year", duration_dict)
+
+        result = assess_station(
+            data;
+            B=B,
+            level=0.05,
+            rng=Xoshiro(seeds[i]),
+        )
+
+        simple_ok[i] = result.SimpleScaling
+        general_ok[i] = result.GeneralScaling
+        simple_pvalue[i] = result.SimpleScalingPValue
+        general_pvalue[i] = result.GeneralScalingPValue
+
+        @info "Completed station" i filename=filenames[i]
+
+    catch err
+        valid[i] = false
+
+        @warn(
+            "Station computation failed",
+            station=filenames[i],
+            exception=(err, catch_backtrace()),
+        )
+    end
+end
